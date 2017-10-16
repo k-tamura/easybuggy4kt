71644f7a92c194a5161d499d7a605ae71e1bcc36
Kohei Tamura <ktamura.biz.80@gmail.com>
2017-10-16 23:13:40 +0900

    Add files via upload

---
+++ src/main/kotlin/org/t246osslab/easybuggy4kt/Easybuggy4ktApplication.kt
@@ -0,0 +1,11 @@
+package org.t246osslab.easybuggy4kt
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Easybuggy4ktApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Easybuggy4ktApplication::class.java, *args)
+}
---
+++ src/test/kotlin/org/t246osslab/easybuggy4kt/Easybuggy4ktApplicationTests.kt
@@ -0,0 +1,16 @@
+package org.t246osslab.easybuggy4kt
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class Easybuggy4ktApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
