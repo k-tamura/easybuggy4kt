ea1a844eb8842ba23d73277a9ec62d2167a3e865
Kohei Tamura <ktamura.biz.80@gmail.com>
2017-10-19 18:33:11 +0900

    Fix a bug that InitListener does not work

--- src/main/kotlin/org/t246osslab/easybuggy4kt/Easybuggy4ktApplication.kt
+++ src/main/kotlin/org/t246osslab/easybuggy4kt/Easybuggy4ktApplication.kt
@@ -1,11 +1,41 @@
 package org.t246osslab.easybuggy4kt
 
+import org.apache.catalina.servlets.DefaultServlet
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.ServletRegistrationBean
+import org.springframework.boot.web.support.SpringBootServletInitializer
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class Easybuggy4ktApplication
+class Easybuggy4sbApplication : SpringBootServletInitializer() {
 
-fun main(args: Array<String>) {
-    SpringApplication.run(Easybuggy4ktApplication::class.java, *args)
+    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+        return application.sources(Easybuggy4sbApplication::class.java)
+    }
+
+    @Bean
+    fun servletRegistrationBean(): ServletRegistrationBean {
+        /* Enable directory listing under /uid/ */
+        val servlet = DefaultServlet()
+        val bean = ServletRegistrationBean(servlet, "/uid/*")
+        bean.isEnabled = true
+        bean.addInitParameter("listings", "true")
+        bean.setLoadOnStartup(1)
+        return bean
+    }
+
+    @Bean
+    fun executorListener(): InitializationListener {
+        return InitializationListener()
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            SpringApplication.run(arrayOf(Easybuggy4sbApplication::class.java, InitializationListener::class.java), args)
+        }
+    }
 }
